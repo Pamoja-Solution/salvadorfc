f6324fb3f734295ac701f12638892d8654b88ce9
Masirika Constantin <elconstant73@gmail.com>
2025-03-14 22:28:46 +0100

    coco2

--- database/migrations/2025_02_23_115213_create_posts_table.php
+++ database/migrations/2025_02_23_115213_create_posts_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name')->unique();
             $table->text('description')->nullable();
-            $table->string('slug')->unique()->after('name'); // Ajoute la colonne `slug` après `name`
+            $table->string('slug')->unique(); // Ajoute la colonne `slug` après `name`
             $table->timestamps();
         });
 
